c84f696a01ce076bd0f563e1208e093b81898078
justine <Justineginchereau@gmail.com>
2018-11-24 20:53:12 -0500

    Saved Sql

---
+++ dbo.InventoryInfo.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[InventoryInfo] (
+    [ProductId]    INT        NOT NULL,
+    [Price]        MONEY NULL,
+    [CurrentStock] INT NULL,
+    PRIMARY KEY CLUSTERED ([ProductId] ASC)
+);
+
